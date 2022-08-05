69c9bd09ecc8d32bc337d9a0ed40602b96b88144
Joey Binas <joseph.t.binas@gmail.com>
2022-08-05 16:51:10 -0700

    created userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+//NEED TO WORK ON GET FRIENDS/REMOVE FRIENDS
+
+// // /api/students/:studentId/assignments
+// router.route('/:studentId/assignments').post(addAssignment);
+
+// // /api/students/:studentId/assignments/:assignmentId
+// router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+
+module.exports = router;
