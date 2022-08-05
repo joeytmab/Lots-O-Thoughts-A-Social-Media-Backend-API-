85fb1c13858b836b913b152b0a14e6d4ae1fc18a
Joey Binas <joseph.t.binas@gmail.com>
2022-08-05 16:51:23 -0700

    created thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+//need to work on add/delete reaction
+
+module.exports = router;
